2536ecdc2d86c50888dc6786f09f53011851c24b
Max Kuhn <mxkuhn@gmail.com>
2017-06-30 17:13:19 -0400

    First pass at mushroom analysis

---
+++ Mushrooms/Templates/C5boostrules_dummy.R
@@ -0,0 +1,75 @@
+library(caret)
+library(C50)
+library(pROC)
+library(ClusterR)
+
+###################################################################
+
+seed <- SEED
+
+data("mushroom")
+mushroom$Over18 <- NULL
+
+###################################################################
+
+
+stats <- function(...) {
+  c(defaultSummary(...),
+    twoClassSummary(...),
+    prSummary(...),
+    mnLogLoss(...))
+}
+
+ctrl <- trainControl(method = "cv", 
+                     classProbs = TRUE,
+                     summaryFunction = stats)
+
+###################################################################
+
+set.seed(seed)
+in_train <- createDataPartition(mushroom$class, p = 3/4, list = FALSE)
+training <- mushroom[ in_train, ]
+testing  <- mushroom[-in_train, ]
+
+mod <- train(class ~ ., data = training, 
+             method = "C5.0",
+             tuneGrid = data.frame(trials = c(1:20, 10*(3:10)),
+                                   model = "rules",
+                                   winnow = FALSE),
+             trControl = ctrl)
+
+###################################################################
+
+test_pred <- predict(mod, testing, type = "prob")
+test_pred$pred <- predict(mod, testing)
+test_pred$obs <- testing$class
+
+test_res <- stats(test_pred, lev = levels(test_pred$obs))
+test_res <- data.frame(t(test_res))
+test_res$Data <- "Mushrooms"
+test_res$Model <- "Boosted C5.0 Rules"
+test_res$Ordered <- "None"
+test_res$Seed <- seed
+test_res$Encoding <- "Dummy Variables"
+test_res$Time <- mod$times$everything[3]
+
+###################################################################
+
+rs_res <- mod$resample
+rs_res$Data <- "Mushrooms"
+rs_res$Model <- "Boosted C5.0 Rules"
+rs_res$Ordered <- "None"
+rs_res$Seed <- seed
+rs_res$Encoding <- "Dummy Variables"
+
+###################################################################
+
+save(test_res, rs_res,
+     file = file.path("..", "Results",
+                      paste0("C5boostrules_dummy_", seed, ".RData")))
+
+###################################################################
+
+sessionInfo()
+
+q("no")
---
+++ Mushrooms/Templates/C5rules_factor.R
@@ -0,0 +1,73 @@
+library(caret)
+library(C50)
+library(pROC)
+library(ClusterR)
+
+###################################################################
+
+seed <- SEED
+
+data("mushroom")
+mushroom$Over18 <- NULL
+
+###################################################################
+
+
+stats <- function(...) {
+  c(defaultSummary(...),
+    twoClassSummary(...),
+    prSummary(...),
+    mnLogLoss(...))
+}
+
+ctrl <- trainControl(method = "cv", 
+                     classProbs = TRUE,
+                     summaryFunction = stats)
+
+###################################################################
+
+set.seed(seed)
+in_train <- createDataPartition(mushroom$class, p = 3/4, list = FALSE)
+training <- mushroom[ in_train, ]
+testing  <- mushroom[-in_train, ]
+
+mod <- train(x = training[, names(training) != "class"], 
+             y = training$class,
+             method = "C5.0Rules",
+             trControl = ctrl)
+
+###################################################################
+
+test_pred <- predict(mod, testing, type = "prob")
+test_pred$pred <- predict(mod, testing)
+test_pred$obs <- testing$class
+
+test_res <- stats(test_pred, lev = levels(test_pred$obs))
+test_res <- data.frame(t(test_res))
+test_res$Data <- "Mushrooms"
+test_res$Model <- "C5.0 Rules"
+test_res$Ordered <- "None"
+test_res$Seed <- seed
+test_res$Encoding <- "Factor Variables"
+test_res$Time <- mod$times$everything[3]
+
+###################################################################
+
+rs_res <- mod$resample
+rs_res$Data <- "Mushrooms"
+rs_res$Model <- "C5.0 Rules"
+rs_res$Ordered <- "None"
+rs_res$Seed <- seed
+rs_res$Encoding <- "Factor Variables"
+
+###################################################################
+
+save(test_res, rs_res,
+     file = file.path("..", "Results",
+                      paste0("C5rules_factor_", seed, ".RData")))
+
+###################################################################
+
+sessionInfo()
+
+q("no")
