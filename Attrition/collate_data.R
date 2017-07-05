ff7286a70e63f37cfb7b78249addc9e93a2a5432
topeop <mxkuhn@gmail.com>
2017-07-05 09:37:37 -0400

    Attrition data runs

--- Attrition/collate_data.R
+++ Attrition/collate_data.R
@@ -1,4 +1,6 @@
 library(dplyr)
+library(tidyr)
+library(ggplot2)
 
 ###################################################################
 
@@ -23,3 +25,34 @@ rs_results <-  bind_rows(rs_results)
 
 ###################################################################
 
+factor_results <- test_results %>%
+  filter(Encoding == "Factor Variables") %>%
+  select(-Encoding) %>%
+  gather(Metric, Factors, Accuracy:logLoss, Time)
+
+
+dummy_results <- test_results %>%
+  filter(Encoding == "Dummy Variables") %>%
+  select(-Encoding) %>%
+  gather(Metric, Dummies, Accuracy:logLoss, Time)
+
+merged_results <- full_join(factor_results, dummy_results)
+
+###################################################################
+
+if(interactive()) {
+  merged_results %>%
+    filter(Metric == "ROC") %>%
+    mutate(Difference = Dummies-Factors) %>%
+    mutate(Model = gsub(" ", "\n", as.character(Model))) %>%
+    mutate(Model = reorder(Model, Difference, median, na.rm = TRUE)) %>%
+    ggplot(aes(x = Model, y = Difference)) + 
+    geom_boxplot()
+}
+
+###################################################################
+
+save(merged_results, file = "Attrition.RData")
+
+if(!interactive()) 
+  q("no")
